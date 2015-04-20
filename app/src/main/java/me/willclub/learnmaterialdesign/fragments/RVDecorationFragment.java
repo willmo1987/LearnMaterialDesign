7978d53a1f58137658a22ea6e3a2ab878cfea71b
Will Mo <willmo1987@gmail.com>
2015-04-20 19:29:43 +0800

    - change name

--- app/src/main/java/me/willclub/learnmaterialdesign/fragments/RVDecorationFragment.java
+++ app/src/main/java/me/willclub/learnmaterialdesign/fragments/RVDecorationFragment.java
@@ -3,7 +3,7 @@ package me.willclub.learnmaterialdesign.fragments;
 /**
  * Created by will on 4/18/15.
  */
-public class RVDecoratorFragment extends BaseFragment {
+public class RVDecorationFragment extends BaseFragment {
 
 
 
